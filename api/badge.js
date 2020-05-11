44c9fcee784b34761f296979ad5fba0f1ad6ea19
Boris Kubiak <kubiakboris@gmail.com>
2020-05-11 11:20:45 +0200

    Use original-url

--- api/badge.js
+++ api/badge.js
@@ -1,15 +1,17 @@
 const badgen = require('badgen')
+const originalUrl = require('original-url')
 
 const CACHE_CONTROL = `public, max-age=60, stale-while-revalidate=86400, stale-if-error=86400`
 
 module.exports = (req, res) => {
   res.setHeader('Cache-Control', `${CACHE_CONTROL}, s-maxage=86400`)
+  const url = originalUrl(req);
 
-  if (req.headers.host.indexOf('api.') === 0) {
+  if (url.hostname.includes('api')) {
     return res.json({ 'subject': 'badges.ml', 'status': 'unavailable', 'color': 'grey' })
   }
 
-  const hostStyle = req.headers.host.indexOf('flat.') === 0 ? 'flat' : undefined
+  const hostStyle = url.hostname.includes('flat') ? 'flat' : undefined
 
   res.setHeader('Content-Type', 'image/svg+xml;charset=utf-8')
   res.send(badgen({
